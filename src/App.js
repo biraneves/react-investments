5ed946e9583a238f5df3876a2f08885e4e6a155e
Ubirajara Neves <ubirajara.neves@usp.br>
2022-08-15 10:22:33 -0300

    Simplified App.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import Header from "./components/Header"
 
 export default function App() {
-    console.log('Teste no console do navegador')
-  
     return (
-        <div>
-            <Header>React Investments</Header>
-        </div>
+        <ReactFundsPage />
     )
   }
